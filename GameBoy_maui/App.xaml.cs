b9f91b6a52e1d5a921d9fe80c8cae6936b39f1ec
metal <metal@PABLO-DESKTOP>
2022-11-26 20:14:49 -0600

    Add project files.

---
+++ GameBoy_maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GameBoy_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ GameBoy_maui/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿namespace GameBoy_maui
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+            byte[] Bytes = GB.loadRom();
+            GB GB_Sys = new GB();
+        }
+
+    }
+
+    public class GB
+    {
+        // Create system hardware
+        struct Registers
+        {
+            byte A;
+            byte B;
+            byte C;
+            byte D;
+            byte E;
+            byte F;
+            byte H;
+            byte L;
+        }
+        private ushort[] memory= null;
+
+        public static byte[] loadRom()
+        {
+            string romPath = @"C:\Users\metal\source\repos\Gameboy\Roms\test\cpu_instrs\individual\06-ld r,r.gb";
+            byte[] Bytes1 = File.ReadAllBytes(romPath);
+
+            Console.Write("hello");
+
+            return Bytes1;
+        }
+
+        public void opcodes()
+        {
+
+        }
+    }
+}
+
+namespace graphics
+{
+    public class GraphicsDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            // Drawing code goes here
+            canvas.FillColor = Colors.Black;
+            canvas.FillRectangle(10, 10, 160, 144);
+        }
+    }
+}
+
