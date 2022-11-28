8852c6e74020f9bf1e153ee2c185c17cc0b72648
pablo <pablocastro079@gmail.com>
2022-11-28 16:43:50 -0600

    Initial opcode work as well as opcode debugger

--- GameBoy_maui/MainPage.xaml.cs
+++ GameBoy_maui/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace GameBoy_maui
 {
@@ -9,70 +10,23 @@ namespace GameBoy_maui
 
 		public MainPage()
 		{
-			InitializeComponent();
+            InitializeComponent();
             string romPath = @"C:\Users\metal\source\repos\Gameboy\Roms\test\cpu_instrs\individual\06-ld r,r.gb";
             byte[] bytes = GB.LoadRom(romPath);
         }
 
-    }
-
-    public static class GB
-    {
-        // Create system hardware
-        public struct Registers
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
-            byte A;
-            byte B;
-            byte C;
-            byte D;
-            byte E;
-            byte F;
-            byte H;
-            byte L;
-            public static ushort PC;
-            ushort SP;
-
-            // Constructor
-            public Registers()
-            {
-                PC = 0x0100;
-            }
-        }
-        //byte[] memory= new byte[0xFFFF]; // 16 bit long memory, each cell 8 bits
-        static byte[] memory = new byte[0xFFFF];
-
-        // Load ROM file as array of bytes (8 bit each)
-        public static byte[] LoadRom(string romPath)
-        {
-
-            byte[] Bytes = File.ReadAllBytes(romPath);
-
-            return Bytes;
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = entry.Text;
         }
 
-        // Return next 8 bits and increment PC
-        public static byte fetchNextByte(byte memory)
+        void OnEntryCompleted(object sender, EventArgs e)
         {
-            byte nextByte = GB.memory[Registers.PC];
-            Registers.PC += 1;
-            return nextByte;
-        }
-
-        public static void RunOpcode(byte opcode)
-        {
-            //Dictionary<int, Action> opcodeTable = new Dictionary<int, Action>();
-
-            switch (opcode)
-            {
-                //NOP
-                // Do nothing
-                case 0x00:
-                    break;
-
-                //LD BC,u16 - 0x01
-                case 0x01:
-                    break;
-            }
+            string text = ((Entry)sender).Text;
+            byte command = Byte.Parse(text);
+            GB.RunOpcode(command);
         }
     }
 }
--- GameBoy_maui/MauiProgram.cs
+++ GameBoy_maui/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace GameBoy_maui;
 
@@ -21,4 +22,159 @@ public static class MauiProgram
 
 		return builder.Build();
 	}
+}
+
+public static class GB
+{
+    // Create system hardware
+    public struct Registers
+    {
+        public static byte A;
+        public static byte B;
+        public static byte C;
+        public static byte D;
+        public static byte E;
+        public static byte F;
+        public static byte H;
+        public static byte L;
+        public static ushort PC;
+        public static ushort SP;
+
+        // Constructor
+        public Registers()
+        {
+            PC = 0x0100;
+        }
+    }
+    //byte[] memory= new byte[0xFFFF]; // 16 bit long memory, each cell 8 bits
+    static byte[] memory = new byte[0xFFFF];
+
+    // Load ROM file as array of bytes (8 bit each)
+    public static byte[] LoadRom(string romPath)
+    {
+
+        byte[] Bytes = File.ReadAllBytes(romPath);
+
+        return Bytes;
+    }
+
+    // Return next 8 bits and increment PC
+    public static byte FetchNextByte()
+    {
+        byte nextByte = memory[Registers.PC];
+        Registers.PC += 1;
+        return nextByte;
+    }
+
+    /// Templates for different opcodes
+    // LD 8-bit Load/Store/Move
+    //private static void LD8Template(var B)
+    //{
+        //System.Diagnostics.Debug.WriteLine("LD BC,u16 - 0x01");
+    //}
+
+    // Increment registers
+    private static void Increment(byte upperByte, byte lowerByte)
+    {
+        ushort pseudo16Bit = (ushort)upperByte;
+        System.Diagnostics.Debug.WriteLine("LD (BC),A - 0x02");
+        //ushort pseudo16Bit = BitConverter.ToInt16(upperByte, lowerByte);
+
+    }
+
+    // Run inputted opcode, return m-cycles opcode takes
+    public static int RunOpcode(byte opcode)
+    {
+        //Dictionary<int, Action> opcodeTable = new Dictionary<int, Action>();
+
+        switch (opcode)
+        {
+            //NOP
+            // Do nothing
+            case 0x00:
+                return 1;
+
+            // LD BC,u16 - 0x01
+            case 0x01:
+                System.Diagnostics.Debug.WriteLine("LD BC,u16 - 0x01");
+                Registers.C = FetchNextByte();
+                Registers.B = FetchNextByte();
+                return 3;
+
+            // LD (BC),A - 0x02
+            case 0x02:
+                System.Diagnostics.Debug.WriteLine("LD (BC),A - 0x02");
+                return 0;
+
+            // INC BC - 0x03
+            case 0x03:
+                System.Diagnostics.Debug.WriteLine("INC BC - 0x03");
+                Increment(Registers.B, Registers.C);
+                return 0;
+
+            // INC B - 0x04
+            case 0x04:
+                System.Diagnostics.Debug.WriteLine("INC B - 0x04");
+                return 0;
+
+            // DEC B - 0x05
+            case 0x05:
+                System.Diagnostics.Debug.WriteLine("DEC B - 0x05");
+                return 0;
+
+            // LD B,u8 - 0x06
+            case 0x06:
+                System.Diagnostics.Debug.WriteLine("LD B,u8 - 0x06");
+                return 0;
+
+            // RLCA - 0x07
+            case 0x07:
+                System.Diagnostics.Debug.WriteLine("RLCA - 0x07");
+                return 0;
+
+            // LD (u16),SP - 0x08
+            case 0x08:
+                System.Diagnostics.Debug.WriteLine("LD (u16),SP - 0x08");
+                return 0;
+
+            // ADD HL,BC - 0x09
+            case 0x09:
+                System.Diagnostics.Debug.WriteLine("ADD HL,BC - 0x09");
+                return 0;
+
+            // LD A,(BC) - 0x0A
+            case 0x0A:
+                System.Diagnostics.Debug.WriteLine("LD A,(BC) - 0x0A");
+                return 0;
+
+            // DEC BC - 0x0B
+            case 0x0B:
+                System.Diagnostics.Debug.WriteLine("DEC BC - 0x0B");
+                return 0;
+
+            // INC C - 0x0C
+            case 0x0C:
+                System.Diagnostics.Debug.WriteLine("INC C - 0x0C");
+                return 0;
+
+            // DEC C - 0x0D
+            case 0x0D:
+                System.Diagnostics.Debug.WriteLine("DEC C - 0x0D");
+                return 0;
+
+            // LD C,u8 - 0x0E
+            case 0x0E:
+                System.Diagnostics.Debug.WriteLine("LD C,u8 - 0x0E");
+                return 0;
+
+            // RRCA - 0x0F
+            case 0x0F:
+                System.Diagnostics.Debug.WriteLine("RRCA - 0x0F");
+                return 0;
+
+            default:
+                System.Diagnostics.Debug.WriteLine("OPCODE: " + opcode + " not implemented!");
+                return 0;
+        }
+    }
 }
